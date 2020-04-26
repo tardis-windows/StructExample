a7048d7a034ff3056a7d776109d06ce511bb6d98
tardis-windows <63209357+tardis-windows@users.noreply.github.com>
2020-04-25 23:46:04 -0400

    Added StructExample.cpp

---
+++ StructExample.cpp
@@ -0,0 +1,45 @@
+#include <iostream> // libraries default 
+
+
+using namespace std;    // std; commands print windows 
+
+
+int main()
+{
+    // all the code 
+    // data types: int char double float boolean 
+
+    int x = 1; 
+    double d; //floating point
+    char c; 
+    bool boo; 
+    
+    cout << "Print to screen" << endl; 
+    cour << "next line" << endl; 
+
+    // convert base, binary to decimal, octal, hexadecimal 
+
+    // control 
+    // if, if-else if-else-if, switch statement 
+
+    if (x == 1)
+    {
+        cout << "Print yes" << endl; 
+    }
+    else
+    {
+        // x not equals to 1
+
+    }
+
+    // loops: for loop and while loop 
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << endl; 
+    }
+
+
+    
+    return 0;
+}
